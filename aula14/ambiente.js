f586e6bb007719594f8c60f7b58b7c9d1c6d5498
ClaudiaSte <grandevale2020@gmail.com>
2026-07-13 19:26:24 -0300

    exercicio novo

---
+++ aula14/ambiente.js
@@ -0,0 +1,21 @@
+/*var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}*/
+
+
+//-------------------------
+
+/*for( var c =1; c <= 5; c++){
+    console.log(c)
+}*/
+
+
+//pode-se usa esse também
+
+console.log('Vai começar...')
+for(var c = 1; c <= 5; c++){
+    console.log(c)
+}
+console.log('FIM!')
